8230961b3aaee93042c39d289ea716ee6c1f47e5
Morgan Matney <morgan.matney89@gmail.com>
2020-04-14 19:56:44 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array) {
+  for (i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+
+function whileLoop(n) {
+  let countdown = n
+  while (n > 0) {
+  console.log(--countdown);
+  }
+  return "done";
+}
